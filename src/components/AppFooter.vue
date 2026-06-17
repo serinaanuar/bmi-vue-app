d4b9880ea254408f902ee404537151436ca4e98f
serinaanuar <intanserinanuar@email.com>
2026-06-18 02:32:56 +0800

    Task 6 - extract sidebar component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footer">
+    <p>© 2026 BMI App | SECJ3483-Web Technology</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+    <h1>BMI Application</h1>
+    <p>Vue CLI Version</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
