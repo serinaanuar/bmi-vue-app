75ced6df88059e87a44f6b69b38ac77f0df17df6
serinaanuar <intanserinanuar@email.com>
2026-06-18 02:53:34 +0800

    Task 9 - integrate person card for last person

---
+++ src/components/PersonCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="single">
+    Name: {{ person.name }},
+    Age: {{ person.age }},
+    Weight: {{ person.weight }} kg,
+    Height: {{ person.height }} cm,
+    BMI: {{ person.bmi }},
+    Category: {{ person.category }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonCard',
+
+  props: {
+    person: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
