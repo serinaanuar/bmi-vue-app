c003f1667ad65fecc5909ade7c415b9c3cc1475b
serinaanuar <intanserinanuar@email.com>
2026-06-18 03:28:09 +0800

    Task 12 - create AddPersonView

---
+++ src/components/EmptyState.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="single">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyState',
+
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
